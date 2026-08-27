a43cba38c12183d8b5bfef7137469b0ca5d67c3b
Ma3a_XaTa <v.radulevich@gmail.com>
2026-08-27 21:57:11 +0300

    gtel3g: exclude unavailable compass feature

--- gtel3g.mk
+++ gtel3g.mk
@@ -72,3 +72,6 @@ PRODUCT_MODEL := SM-T561
 PRODUCT_PACKAGES += \
     font_log.png \
     sprd_charger_animation
+
+# Excluded hardware features
+PRODUCT_COPY_FILES += device/samsung/gtel3g/configs/permissions/gtel3g_excluded_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/gtel3g_excluded_hardware.xml
